2a749728185bc1bccf4bf5b373f7edf5d01c0f11
eslan94 <eslan94.eca@gmail.com>
2024-02-04 22:39:02 -0500

    Screen Registro

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,33 +1,39 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 
 export const RegisterScreen = () => {
 
-    const [usuario, onChangeUsuario] = useState('');
-    const [clave, onChangeClave] = useState('');
+    const [nombre, onChangeNombre] = useState('');
+    const [apellido, onChangeApellido] = useState('');
+    const [correo, onChangeCorreo] = useState('')
+    const[clave, onChangeClave] = useState('')
 
   return (
     <View style={styles.container}>
         <View>
-        <Text style={styles.texto}>Usuario</Text>
-        <TextInput style={styles.input} onChangeText={onChangeUsuario} value={usuario} placeholder='usuario'/>
+            <Text style={styles.title}>Registro Usuario</Text>
         </View>
         <View>
-        <Text style={styles.texto}>Usuario</Text>
-        <TextInput style={styles.input} onChangeText={onChangeUsuario} value={usuario} placeholder='usuario'/>
+            <Text style={styles.texto}>Nombre</Text>
+            <TextInput style={styles.input} onChangeText={onChangeNombre} value={nombre} placeholder='Nombre'/>
         </View>
         <View>
-        <Text style={styles.texto}>Usuario</Text>
-        <TextInput style={styles.input} onChangeText={onChangeUsuario} value={usuario} placeholder='usuario'/>
+            <Text style={styles.texto}>Apellido</Text>
+            <TextInput style={styles.input} onChangeText={onChangeApellido} value={apellido} placeholder='Apellido'/>
         </View>
         <View>
-        <Text style={styles.texto}>Usuario</Text>
-        <TextInput style={styles.input} onChangeText={onChangeUsuario} value={usuario} placeholder='usuario'/>
+            <Text style={styles.texto}>Correo</Text>
+            <TextInput style={styles.input} onChangeText={onChangeCorreo} value={correo} placeholder='Correo'/>
         </View>
         <View>
-        <Text style={styles.texto}>Usuario</Text>
-        <TextInput style={styles.input} onChangeText={onChangeUsuario} value={usuario} placeholder='usuario'/>
+            <Text style={styles.texto}>Contraseña</Text>
+            <TextInput style={styles.input} onChangeText={onChangeClave} value={clave} placeholder='Contraseña' secureTextEntry={true}/>
         </View>
+        <View style={styles.boton}>
+        <TouchableOpacity style={styles.cajaBoton} >
+          <Text style={styles.textoBoton}>Registrar</Text>
+        </TouchableOpacity>
+      </View>  
     </View>
   )
 }
@@ -45,8 +51,28 @@ const styles = StyleSheet.create({
       borderRadius:7,
       padding:10
     },
+    title:{
+        marginVertical:20,
+        fontSize:40,
+        textAlign:'center',
+        color: '#0824F2'
+    },
     texto:{
       marginLeft:20,
       fontSize: 20
     },
+    boton:{
+        alignItems:'center',
+      },
+      cajaBoton:{
+        backgroundColor: '#525FE1',
+        paddingVertical: 20,
+        width:150,
+        marginTop:20
+      },
+      textoBoton:{
+        textAlign:'center',
+        color:'white',
+        fontSize: 25
+      },
 })
